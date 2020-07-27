c7784d8797d779ca0d9995f726abf3d16372bc18
HyunsuYu <63137431+HyunsuYu@users.noreply.github.com>
2020-07-27 23:19:58 +0900

    Temp Update

---
+++ FirebaseStoragePathManagerDll/main.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FirebaseStoragePathManager
+{
+    #region Defines
+    
+    #endregion
+
+    #region MainClass
+    public class FirebaseStoragePathManager_Formal<DetailFileType>
+    {
+        public enum AbstractFileType
+        {
+            File = 1,
+            Directory = 2
+        };
+
+        public struct Node
+        {
+            public string mname;
+            public string mpath;
+            public AbstractFileType mabstractFileType;
+            public DetailFileType mdetailFileType;
+
+            public Dictionary<string, Node> childBranch;
+
+            public Node DefaultInstance
+            {
+                get => new Node();
+            }
+        }
+    }
+    #endregion
+}
